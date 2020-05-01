878b551881b63cd5ad7ab7d6b13af567344a6bca
Glitch (enthusiastic-melted-trader) <none>
2020-05-01 03:40:57 +0000

    🌷🏦 Updated change validation in controller to user.validation

---
+++ validation/user.validation.js
@@ -0,0 +1,11 @@
+module.exports.postUser = (req, res, next) => {
+  let data = req.body;
+	// lọc tên sai quy dinh
+	if(data.name.length > 30 || /^\s+$/.test(data.name)) {
+		res.render('user/addUser.pug', {
+			error: "Name only start 'a - z, A - Z, 0 - 9' and < 30 character"
+		});
+		return;
+	}
+  next();
+}
