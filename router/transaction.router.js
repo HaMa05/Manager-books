33325e445c0f37342991f50c835446223440432f
Glitch (rainbow-seemly-maxilla) <none>
2020-05-03 17:28:48 +0000

    🙎👸 Updated Feature/ Login (User || Admin)

--- router/transaction.router.js
+++ router/transaction.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const controllerTransaction = require('../controllers/transaction.controller.js');
 
-router.get('/', controllerTransaction.indexTransaction);
+router.get("/", controllerTransaction.indexTransactionUser, controllerTransaction.indexTransaction);
 
 router.get('/create', controllerTransaction.create);
 
